e9a82ace6721b92411fef187c244a442c715f091
Darius Cox <ddcgamer243@gmail.com>
2019-03-25 14:55:23 -0400

    lab start state

--- server.js
+++ server.js
@@ -10,12 +10,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.post("/api/killme", (req, res) => {
   console.log("Received a request", req);
   if (featureFlag) {
-    res.status(200);
-    res.send("Hey you didn't break the server this time!");
-  } else {
     res.status(505);
-    res.send("Whoops - You killed the server :(");
+    res.send("Whoops - Server doggo is borked :(");
+  } else {
     //process.exit(1);
+    res.status(200);
+    res.send("Hey you didn't bork the server this time!");
   }
 });
 
