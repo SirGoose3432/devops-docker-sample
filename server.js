b52c758ad4e1a0bde325c9310ecc72c03a334514
Johann Voelkening <jvoelkening@gmail.com>
2019-03-25 00:43:44 -0400

    Changed to single server for prod

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const path = require("path");
 const app = express();
-const port = process.env.PORT || 5000;
-const featureFlag = process.env.AB_TEST || false;
+const port = 8080;
+const featureFlag = process.env.flag || false;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -16,6 +17,12 @@ app.post("/api/killme", (req, res) => {
     res.send("Whoops - You killed the server :(");
     process.exit(1);
   }
+});
+
+app.use(express.static(path.join(__dirname, "build")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
